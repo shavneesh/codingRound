d4c8d82fffd84efeef34eaac84782e11b8d69974
shavneesh <58099994+shavneesh@users.noreply.github.com>
2019-11-23 19:53:25 +0530

    added java docs for the method

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -1,5 +1,6 @@
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.openqa.selenium.By;
@@ -12,11 +13,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.sun.javafx.PlatformUtil;
 
 /*
- *this class contains all the method that are used or might be use in test classes 
+ *This class contains all the method that are used or might be use in test classes 
  */
 public class BaseClass {
 	public WebDriver driver = null;
 
+	/**
+	 * This method is for the static wait
+	 * 
+	 * @param durationInMilliSeconds
+	 *            - need to provide int value for time
+	 */
 	public void waitFor(int durationInMilliSeconds) {
 		try {
 			Thread.sleep(durationInMilliSeconds);
@@ -25,19 +32,29 @@ public class BaseClass {
 		}
 	}
 
+	/**
+	 * This method will verify weather an element is present in the DOM or not It
+	 * return true if element is present and false if element is not present
+	 * 
+	 * @param by
+	 *            - need to pass the locator with locator type
+	 * @return
+	 */
 	public boolean isElementPresent(By by) {
 		try {
 			driver.findElement(by);
 			return true;
 		} catch (NoSuchElementException e) {
+			System.out.println(e.getMessage() + " element is not found");
 			return false;
 		}
 	}
 
-	/*
-	 * changed the return type of the method to webDriver
-	 * initials the chormeDriver
-	 * return the driver
+	/**
+	 * This method will check which operating system is use for the test Set the
+	 * path of chormedriver
+	 * 
+	 * @return - this method will return the reference of chormedriver
 	 */
 	public WebDriver setDriverPath() {
 		if (PlatformUtil.isMac()) {
@@ -53,31 +70,33 @@ public class BaseClass {
 		return driver;
 	}
 
-	/*
-	 * this method accept two parameter one is the locator and second the type of locator
-	 * according to the locator type appropriate explicit wait will be selected
-	 * this method will wait for 20 second for a element to be visible on web page
+	/**
+	 * According to the locator type appropriate explicit wait will be selected This
+	 * method will wait for 20 second for a element to be visible on web page
+	 * 
+	 * @param locator-
+	 *            Pass locator as a string
+	 * @param locatorType
+	 *            - type of locator use for locating the element
 	 */
-	public void waitElementToBeVisible(String e, String locator) {
-		if (locator == "css") {
+	public void waitElementToBeVisible(String locator, String locatorType) {
+		if (locatorType == "css") {
 			WebDriverWait wait = new WebDriverWait(driver, 20);
-			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(e)));
-		}
-		if (locator == "xpath") {
+			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(locator)));
+		} else if (locatorType == "xpath") {
 			WebDriverWait wait = new WebDriverWait(driver, 20);
-			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(e)));
+			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(locator)));
 		}
 	}
 
-	/*/
-	 * This method will select the current date from the calendar
-	 * this resolve the static reference to the calendar date
+	/**
+	 * This method will select the current date from the calendar. 
+	 * This remove the static reference to the calendar date
 	 */
 	public void selectDate() {
 
 		Date currentDate = new Date();
 
-		SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
 		String day = new SimpleDateFormat("dd").format(currentDate);
 
 		waitElementToBeVisible("//a[text()='" + day + "']", "xpath");
--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -15,58 +15,80 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-//extended baseclass to increase the usability
+//Extended baseclass to increase the usability
 
 public class FlightBookingTest extends BaseClass {
 
+	@Test
+	public void testThatResultsAppearForAOneWayJourney() {
 
-    @Test
-    public void testThatResultsAppearForAOneWayJourney() {
+		setDriverPath();
 
-        setDriverPath();
-        
-      //added implicit wait
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        
-        driver.get("https://www.cleartrip.com/");
-        
-        driver.findElement(By.id("OneWay")).click();
+		// Added implicit wait
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-        driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+		driver.get("https://www.cleartrip.com/");
 
-        //wait for the auto complete options to appear for the origin
-        //added explicit wait for the auto suggestions
-        waitElementToBeVisible("#ui-id-1 li a","css");
-        
-        List<WebElement> originOptions = driver.findElements(By.cssSelector("#ui-id-1 li a"));
-        originOptions.get(0).click();
+		driver.findElement(By.id("OneWay")).click();
 
-        driver.findElement(By.id("ToTag")).clear();
-        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
+		driver.findElement(By.id("FromTag")).clear();
+		driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
-        //wait for the auto complete options to appear for the destination
-        //added explicit wait for the auto suggestions
-        waitElementToBeVisible("#ui-id-2 li a","css");
-        //select the first item from the destination auto complete list
-        List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("a"));
-        destinationOptions.get(0).click();
+		// Wait for the auto complete options to appear for the origin
+		// Added explicit wait for the auto suggestions
+		waitElementToBeVisible("#ui-id-1 li a", "css");
 
-        //driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
-        //created a new method to select the date from the calendar
-        selectDate();
+		List<WebElement> originOptions = driver.findElements(By.cssSelector("#ui-id-1 li a"));
+		
+		/*
+		 * Below statement check null exception for the list
+		 * Click operation will only form when list is not empty
+		 */
+		if (!originOptions.isEmpty()) {
+			originOptions.get(0).click();
+		} else {
+			Assert.assertFalse(true, "List of auto suggestion not element for the originOptions field ");
+		}
 
-        //all fields filled in. Now click on search
-        driver.findElement(By.id("SearchBtn")).click();
+		driver.findElement(By.id("ToTag")).clear();
+		driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
-        waitElementToBeVisible(".searchSummary","css");
-        //verify that result appears for the provided journey search
-        Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
-        //close the browser
-        driver.quit();
+		/*
+		 * Wait for the auto complete options to appear for the destination
+			Added explicit wait for the auto suggestions
+		 */
+		waitElementToBeVisible("#ui-id-2 li a", "css");
+		
+		// select the first item from the destination auto complete list
+		List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("a"));
+		
+		/*
+		 * Below statement check null exception for the list
+		 * Click operation will only form when list is not empty
+		 */
+		if(!destinationOptions.isEmpty()) {
+			destinationOptions.get(0).click();
+		}else {
+			Assert.assertFalse(true, "List of auto suggestion not element for the destinationOptions field ");
+		}
+		
 
-    }
+		// Driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+		
+		// This method will select the current date from the calendar
+		selectDate();
 
+		// All fields filled in. Now click on search
+		driver.findElement(By.id("SearchBtn")).click();
+
+		waitElementToBeVisible(".searchSummary", "css");
+		// Verify that result appears for the provided journey search
+		Assert.assertTrue(isElementPresent(By.className("searchSummary")));
+
+		// Close the browser
+		driver.quit();
+
+	}
 
 }
